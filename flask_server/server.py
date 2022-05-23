26935e23c004b58b504fca5c12bccb8eb5b614f4
Shaden27 <shreepad.divekar@gmail.com>
2022-05-23 14:26:48 +0530

    Cloud Test2

--- flask_server/server.py
+++ flask_server/server.py
@@ -663,7 +663,9 @@ def sendScan1():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 # https://shreepadtest.herokuapp.com/
